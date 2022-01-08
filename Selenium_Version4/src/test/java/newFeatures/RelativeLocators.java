2fb2749aff53ec98d2d35691a38d04b5c247d3e6
Deo Prasad Shaw <deoshaw3@gmail.com>
2022-01-08 11:27:56 +0530

    modified script

--- Selenium_Version4/src/test/java/newFeatures/RelativeLocators.java
+++ Selenium_Version4/src/test/java/newFeatures/RelativeLocators.java
@@ -1,11 +1,9 @@
 package newFeatures;
 
-import java.io.File;
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.locators.RelativeLocator;
 
@@ -18,15 +16,20 @@ public class RelativeLocators {
 	{
 	//System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+File.separator+"chromedriver.exe");
 	//WebDriver driver=new ChromeDriver();
-		WebDriverManager.chromedriver().version("94.0.4606.61").setup();
+		//WebDriverManager.chromedriver().version("96.0.4664.110").setup();
+		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver=new ChromeDriver();
 	driver.get("http://softwaretestingplace.blogspot.com/2015/10/sample-web-page-to-test.html");
 	driver.manage().window().maximize();
 	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
 	
 	driver.findElement(RelativeLocator.with(By.xpath("//input[@name='lastname']")).below(By.xpath("//*[@name='firstname']"))).sendKeys("Shaw");
+	//driver.findElement(By.xpath("//input[@name='lastname']")).sendKeys("Shaw");
 	Thread.sleep(5000);
 	driver.quit();
+	
+	
+	
 	}
 
 }
