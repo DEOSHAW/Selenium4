0a9e7ee733089c4625906ad50c12a14b2fbf500c
Deo Prasad Shaw <deoshaw3@gmail.com>
2022-03-26 10:38:40 +0530

    Added set demo class

---
+++ Selenium_Version4/src/test/java/javaFeatures/SetDemo.java
@@ -0,0 +1,25 @@
+package javaFeatures;
+
+import java.util.*;
+import java.util.Set;
+
+public class SetDemo {
+	
+	
+        public static void main(String args[])
+         {
+        	
+        	HashMap<Integer,String> map=new HashMap<Integer,String>();
+        	map.put(1454080, "Deo Prasad Shaw");
+        	map.put(1454760, "Aman Bhagat");
+        	
+        	Set<Integer> keys=map.keySet();
+        	for(Integer i:keys)
+        	{
+        		System.out.println(map.get(i));
+        	
+         }
+
+
+     }
+}
