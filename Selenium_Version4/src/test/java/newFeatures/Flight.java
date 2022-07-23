2182273d27293ed2bed4fe40bbc34643c0ebd522
Deo Prasad Shaw <deoshaw3@gmail.com>
2022-07-23 06:44:28 +0530

    Modified webdriver manager test

--- Selenium_Version4/src/test/java/newFeatures/Flight.java
+++ Selenium_Version4/src/test/java/newFeatures/Flight.java
@@ -17,7 +17,7 @@ public class Flight {
 
 		public static void main (String[] args) throws InterruptedException
 		{
-			WebDriverManager.chromedriver().setup();
+		WebDriverManager.chromedriver().version("103.0.5060.134").setup();
 		//System.setProperty("webdriver.chrome.driver", "C:/Users/Bishal/chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
